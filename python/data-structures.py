97ba63e2d2c72d281d0ab07375250a0ecfc05ca8
Kenneth Smith <76234154+mrsmyth-git@users.noreply.github.com>
2021-08-07 13:12:47 +0000

    set notes in python

--- python/data-structures.py
+++ python/data-structures.py
@@ -1,14 +1,15 @@
 # Can contain multiple values but most of the time contain one value
-list = ('anyvalue', 234, True)
+list = ['anyvalue', 234, True]
 
 print(list[2])
 
 # Can contain multiple values
-tuple = 234, 'hello', 234234
+tuple = (234, 'hello', 234234)
 
 print(tuple[1])
 
 # Any data repeated in a set will not be saved
+my_set = set()
 set = {'one', 'value', 'at', 'at', 'a', 'a', 'time'}
 
 print(set[0])
